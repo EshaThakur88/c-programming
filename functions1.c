3a6ef6eafb89da1f1ea432d8dca061e5c91ad914
Esha Thakur <eshathakur888@gmail.com>
2021-08-13 21:10:54 +0530

    go

---
+++ functions1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int sum(int a,int b){ 
+  return a+b;
+}
+
+int main(){
+    int a,b,c;
+    a=7;
+    b=9;
+    c=sum(a,b);
+    printf("the sum is %d",c);
+    return 0;
+    }
