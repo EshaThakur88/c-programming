74ffb7f8d7de2b6bfe23c316b4eb4ebc2785aa90
Esha Thakur <eshathakur888@gmail.com>
2021-09-04 02:16:17 +0530

    go

---
+++ q7.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+
+    int sum = 0;
+    for (int i = 1; i <= 10; i++)
+    {
+        sum += 8 * i;
+    }
+
+    printf("Sum of numbers occuring in the multiplication table of 8 is: %d", sum);
+
+    return 0;
+}
